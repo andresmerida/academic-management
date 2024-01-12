9a5246dd46a29dfe239793855a24119c70d39d1c
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2024-01-12 10:58:24 -0400

    Adding crud services for area

--- am-core-web-service/src/main/java/org/am/core/web/repository/jpa/admingeneral/AreaRepository.java
+++ am-core-web-service/src/main/java/org/am/core/web/repository/jpa/admingeneral/AreaRepository.java
@@ -1,7 +1,10 @@
 package org.am.core.web.repository.jpa.admingeneral;
 
 import org.am.core.web.domain.entity.admingeneral.Area;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AreaRepository extends CrudRepository<Area, Integer> {
+import java.util.List;
+
+public interface AreaRepository extends JpaRepository<Area, Integer> {
+    List<Area> findAllByActiveOrderByName(Boolean active);
 }
