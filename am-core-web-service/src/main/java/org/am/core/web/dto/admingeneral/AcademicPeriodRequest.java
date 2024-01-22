bd20072c0cd193cb744f10c2cb8f19ea40c30bb7
miguelC1 <miguel68933886@gmail.com>
2024-01-22 08:49:03 -0400

    creacion de CRUD de periodo Academico,y script de la tabla Periodo Academico

---
+++ am-core-web-service/src/main/java/org/am/core/web/dto/admingeneral/AcademicPeriodRequest.java
@@ -0,0 +1,7 @@
+package org.am.core.web.dto.admingeneral;
+
+import java.time.LocalDate;
+import java.time.Year;
+
+public record AcademicPeriodRequest(Year year, String name, LocalDate startDate, LocalDate endDate, Boolean active){
+}
