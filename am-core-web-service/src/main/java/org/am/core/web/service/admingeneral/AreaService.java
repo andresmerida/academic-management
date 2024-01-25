ad93280cae0fe07c95f9a7aac7cbaa681d429134
Andres Merida <andresmerida1@gmail.com>
2024-01-25 10:31:20 -0400

    Adding schedule parameters service and swagger (#9)
    Co-authored-by: Andres Merida <andresmerida@Andress-MacBook-Pro.local>

--- am-core-web-service/src/main/java/org/am/core/web/service/admingeneral/AreaService.java
+++ am-core-web-service/src/main/java/org/am/core/web/service/admingeneral/AreaService.java
@@ -1,24 +1,35 @@
 package org.am.core.web.service.admingeneral;
 
 import org.am.core.web.domain.entity.admingeneral.Area;
+import org.am.core.web.domain.entity.admingeneral.AreaParameters;
 import org.am.core.web.dto.admingeneral.AreaDto;
 import org.am.core.web.dto.admingeneral.AreaRequest;
 import org.am.core.web.repository.jpa.CustomMap;
+import org.am.core.web.repository.jpa.admingeneral.AreaParametersRepository;
 import org.am.core.web.repository.jpa.admingeneral.AreaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static org.am.core.web.util.UtilConstants.DEFAULT_END_TIME_SCHEDULE;
+import static org.am.core.web.util.UtilConstants.DEFAULT_START_TIME_SCHEDULE;
+import static org.am.core.web.util.UtilConstants.DEFAULT_TIME_INTERVAL_SCHEDULE;
+
 @Service
+@Transactional
 public class AreaService implements CustomMap<AreaDto, Area> {
     private final AreaRepository areaRepository;
+    private final AreaParametersRepository areaParametersRepository;
 
-    public AreaService(AreaRepository areaRepository) {
+    public AreaService(AreaRepository areaRepository, AreaParametersRepository areaParametersRepository) {
         this.areaRepository = areaRepository;
+        this.areaParametersRepository = areaParametersRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<AreaDto> findAllActive() {
         return areaRepository.findAllByActiveOrderByName(Boolean.TRUE)
                 .stream()
@@ -26,7 +37,23 @@ public class AreaService implements CustomMap<AreaDto, Area> {
     }
 
     public AreaDto save(AreaRequest areaRequest) {
-        return toDto(areaRepository.save(this.toEntity(areaRequest)));
+        Area areaPersisted = areaRepository.save(this.toEntity(areaRequest));
+        // Init with default values
+        AreaParameters areaParameters = new AreaParameters(
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                false,
+                DEFAULT_START_TIME_SCHEDULE,
+                DEFAULT_END_TIME_SCHEDULE,
+                DEFAULT_TIME_INTERVAL_SCHEDULE,
+                areaPersisted
+        );
+        areaParametersRepository.save(areaParameters);
+        return toDto(areaPersisted);
     }
 
     public AreaDto edit(AreaDto areaDto) {
@@ -37,11 +64,13 @@ public class AreaService implements CustomMap<AreaDto, Area> {
         return toDto(areaRepository.save(areaFromDB));
     }
 
+    @Transactional(readOnly = true)
     public Optional<AreaDto> getAreaById(Integer id) {
         return areaRepository.findById(id).map(this::toDto);
     }
 
     public void delete(Integer id) {
+        areaParametersRepository.deleteById(id);
         areaRepository.deleteById(id);
     }
 
