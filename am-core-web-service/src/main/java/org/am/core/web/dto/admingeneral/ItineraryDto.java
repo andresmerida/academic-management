eb9bc4b4f250a373e8453650f148efef5acb19ab
DafneXCA <cruzdafne123@gmail.com>
2024-02-14 19:44:05 -0400

    merge dev

--- am-core-web-service/src/main/java/org/am/core/web/dto/admingeneral/ItineraryDto.java
+++
@@ -1,6 +0,0 @@
-package org.am.core.web.dto.admingeneral;
-
-import org.am.core.web.domain.entity.admingeneral.Career;
-
-public record ItineraryDto(Integer id, String name, Integer careerId,String careerName ) {
-}
